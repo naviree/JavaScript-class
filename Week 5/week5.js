8238c000e36f01cdf715f64994a7614ab9ac203d
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-02-16 11:14:06 -0800

    continuing to work on arrays

--- Week 5/week5.js
+++ Week 5/week5.js
@@ -1,5 +1,51 @@
-function init() {
-
+var VALUES = [
+    0,
+    1,
+    "0",
+    "1",
+    NaN,
+    Infinity,
+    -Infinity,
+    null,
+    undefined,
+    {},
+    {},
+    [],
+    [],
+    [0, 1, 2],
+    {
+        vegetable: "asparagus",
+        meat: "steak"
+    },
+    console,
+    console.log,
+    document,
+    document.write
+];
+function stringValue(value) {
+    return value;
+}
+function generateTypeHeader() {
+    return "<tr><th>Value</th><th>Type</th>\r\n";
 }
 
-window.addEventListener('load, init');
+function generateTypeRow(value) {
+    return "<tr><td>" + stringValue(value) + "</td><td>" + (typeof value) + "</td></tr>\r\n";
+}
+
+function generateTypeTable() {
+    table = "<table>\r\n";
+
+    table += " " + generateTypeHeader();
+    for (var counter = 0; counter < VALUES.length; counter++) {
+        table += " " + generateTypeRow(VALUES[counter]);
+    }
+    table += "</table>\r\n";
+    return table;
+}
+
+function init() {
+    console.log("Welcoem to week 5")
+}
+
+window.addEventListener('load', init);
